a6a504e0898be6358dc5db3144e54b661a1dbfbb
bvv94 <bvdevasconcelos@gmail.com>
2023-09-28 16:52:56 -0400

    FEAT Abrir urls encurtadas

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -1,6 +1,7 @@
 import { nanoid } from "nanoid";
 import { db } from "../database/database.connection.js";
 import { v4 as uuidv4 } from "uuid";
+import { Express } from "express";
 
 export async function shorten(req, res) {
     const { url } = req.body;
@@ -24,20 +25,44 @@ export async function shorten(req, res) {
 
 export async function getUrls(req, res) {
 
-    const {id} = req.params
+    const { id } = req.params
 
     const exists = await db.query(`SELECT * FROM urls WHERE id=$1;`, [id])
-    if(!exists) return res.status(404).send("Id não existe!");
+    if (!exists.rows.length) return res.status(404).send("URL não encontrada");
 
     try {
         const urls = await db.query(`SELECT (id, short_url, url) FROM urls WHERE id=$1;`, [id]);
-        console.log(url);
+        console.log(urls);
 
-        const {id, shortUrl, url} = urls.rows[0];
+        const { id, short_url, url } = urls.rows[0];
 
-        const formatted = {...formatted, id, shortUrl, url}
+        const formatted = { id, short_url, url }
 
         res.status(200).send(formatted)
+    }
+    catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function openShortUrl(req, res) {
+    const { shortUrl } = req.params;
+    
+    // - Redirecionar o usuário para o link correspondente.
+    // - Aumentar um na contagem de visitas do link.
+    // - Caso a url encurtada não exista, responder com *status code* `404`.
+    
+    const file = db.query(`SELECT * FROM urls WHERE short_url = $1;`, [shortUrl]);
+    if (!file.rows.length) return res.status(404).send("URL não encontrada");
+
+    let count = file.rows[0].visit_count + 1;
+    
+    try {        
+        await db.query(`UPDATE urls SET visit_count = $1 WHERE short_url = $2;`, [count, shortUrl]);
+        res.redirect(file.rows[0].url);
+
+        // res.writeHead(301, { Location: file.rows[0].url });
+        // res.end();
     }
     catch (err) {
         res.status(500).send(err.message);
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import validateAuth from "../middlewares/validateAuth.middlewares.js";
-import { shorten } from "../controllers/urls.controllers.js";
+import { getUrls, openShortUrl, shorten } from "../controllers/urls.controllers.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", validateAuth, shorten);
-urlRouter.get("/urls/:id", validateAuth, shorten);
-urlRouter.get ("/urls/open/:shortUrl",)
+urlRouter.get("/urls/:id", validateAuth, getUrls);
+urlRouter.get ("/urls/open/:shortUrl", openShortUrl)
 urlRouter.delete("/urls/:id")
 
 export default urlRouter;
