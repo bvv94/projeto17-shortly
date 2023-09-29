5079af04d187d14ab8008244a403c96f5eef567a
bvv94 <bvdevasconcelos@gmail.com>
2023-09-29 16:24:37 -0400

    FEAT Dados do usuário Finalizada

--- src/controllers/me.controller.js
+++ src/controllers/me.controller.js
@@ -4,15 +4,25 @@ export default async function me(req, res) {
     const { user_id } = res.locals.session;
 
     try {
-        const totalVisits = await db.query(`
+        const totalVisitsURLS = await db.query(`
             SELECT SUM(visit_count) AS "visitCount"
                 FROM urls WHERE user_id = $1;`, [user_id]);
+        const totalVisits = totalVisitsURLS.rows[0].visitCount;
+        
+        const resultName = await db.query(`SELECT name FROM users WHERE id = $1;`, [user_id]);
+        const name = resultName.rows[0].name;
 
-        const { name } = await db.query(`SELECT name FROM users WHERE id = $1;`, [user_id]);
-
-        const shortenedUrls = await db.query(`
+        const shortenedUrlsResult = await db.query(`
             SELECT id, short_url AS "shortUrl", url, visit_count AS "visitCount"
                 FROM urls WHERE user_id = $1;`, [user_id]);
+        const shortenedUrls = shortenedUrlsResult.rows;
+
+        const formatted = {
+            id: user_id,
+            name: name,
+            visitCount: totalVisits,
+            shortenedUrls: shortenedUrls
+        }
 
         res.status(200).send(formatted);
     }
